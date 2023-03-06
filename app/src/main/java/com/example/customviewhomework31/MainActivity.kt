1d9ab15105258f954ccd8117b1c90aac104f8990
Zanimonetc Mikhail <z_a_r_k@mail.ru>
2023-03-06 19:33:03 +0300

    Classwork 31

--- app/src/main/java/com/example/customviewhomework31/MainActivity.kt
+++ app/src/main/java/com/example/customviewhomework31/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.customviewhomework31
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +16,21 @@ class MainActivity : AppCompatActivity() {
 
         val counter = findViewById<CounterView>(R.id.counter_view)
         val button = findViewById<Button>(R.id.button_increase_decrease).apply { text = counter.buttonCondition.toString() }
+//        val animator = ValueAnimator.ofFloat(1f, 0f)
+//        animator.addUpdateListener { animation ->
+//            counter.alpha = (animation.animatedValue as Float)
+//        }
+//        animator.duration = 2000
+        val set = AnimationUtils.loadAnimation(this,R.anim.rotation_and_skale_set_animation)
+        set.fillAfter = true
         button.setOnClickListener{
-            button.apply { text = counter.changeButton() }
-            counter.switch()
+//            button.apply { text = counter.changeButton() }
+//            counter.switch()
+//            ObjectAnimator.ofFloat(counter, View.ROTATION, 0f, 360f).start()
+//            animator.start()
+//            counter.animate().rotationBy(0f).rotation(360f).start()
+//            counter.startAnimation(set)
+            startActivity(Intent(this, SecndActivity::class.java))
         }
     }
 }
